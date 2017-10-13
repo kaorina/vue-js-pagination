6951a1a7028813a5e4a69f8cd245713563f91a95
朝倉 香里 <kaori@asakura-no-MacBook-Air.local>
2017-10-13 15:15:26 +0900

    ページネーションを実装

--- app/assets/javascripts/components/pagination.js
+++ app/assets/javascripts/components/pagination.js
@@ -1,7 +1,31 @@
+var paginationButton = {
+  'first': { '1st': false, '2nd': true, '3rd': false, '4th': false },
+  'second': { '1st': true, '2nd': false, '3rd': true, '4th': false },
+  'third': { '1st': true, '2nd': true, '3rd': false, '4th': true },
+  'fourth': { '1st': true, '2nd': true, '3rd': true, '4th': false }
+};
+
 Vue.component('pagination', {
   template: '#pagination-template',
+  props: {
+    pageState: String
+  },
+  computed: {
+    firstButton: function () {
+      return paginationButton[this.pageState]['1st'];
+    },
+    secondButton: function () {
+      return paginationButton[this.pageState]['2nd'];
+    },
+    thirdButton: function () {
+      return paginationButton[this.pageState]['3rd'];
+    },
+    fourthButton: function () {
+      return paginationButton[this.pageState]['4th'];
+    }
+  },
   methods: {
-    changeState: function (page) {
+    changePage: function (page) {
       this.$emit('send-page-from-child', page);
     }
   }
